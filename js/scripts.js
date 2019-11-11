7bfe2bd42acb4b746a9312363af3f67ef374c8ab
Geoffrey Orengo <geoff.orengo@yahoo.com>
2019-11-10 19:08:42 -0800

    add to scripts file for functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 //Business Logic
-var output = [];
-
 function beepBoop(number) {
+    var output = [];
     for (i=0; i<= number; i++) {
       output.push(i);
     console.log(i);
     console.log(output);
   }
-  if (i=1 ) {
-    
+  if (i===3 ) {
+    let i = ("I'm sorry Dave, I can't do that.");
+  }
+  else if (i===2) {
+    let i = "Boop";
+  }
+  else if (i===1) {
+    let i = "Beep";
   }
 };
 
